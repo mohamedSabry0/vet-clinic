ac2430b8476a5b06272e1550597e71bdaeedf42c
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-06-16 19:47:47 +0300

    Add tables, Update columns and relations

--- schema.sql
+++ schema.sql
@@ -13,3 +13,25 @@ CREATE TABLE animals(
 
 ALTER TABLE animals
 ADD COLUMN species varchar(50);
+
+-- ==================================================
+
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY NOT NULL,
+    full_name VARCHAR(50),
+    age INTEGER
+);
+
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(50)
+);
+
+ALTER TABLE animals
+DROP COLUMN species;
+
+ALTER TABLE animals
+ADD COLUMN species_id INTEGER REFERENCES species(id);
+
+ALTER TABLE animals
+ADD COLUMN owner_id INTEGER REFERENCES owners(id);
