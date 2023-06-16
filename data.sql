d5c2e0c803807f0c9ac8b255f827b1c84d897650
mohamedSabry0 <mohamedsabry3023@gmail.com>
2023-06-16 17:21:36 +0300

    Fix naming on data.sql file

--- data.sql
+++ data.sql
@@ -1,10 +1,6 @@
 /* Populate database with sample data. */
 
--- INSERT INTO animals (name) VALUES ('Luna');
--- INSERT INTO animals (name) VALUES ('Daisy');
--- INSERT INTO animals (name) VALUES ('Charlie');
-
-INSERT INTO animal (name, date_of_birth, weight_kg, neutered, escape_attempts)
+INSERT INTO animals (name, date_of_birth, weight_kg, neutered, escape_attempts)
 VALUES
 ('Agumon', '2020-02-03', 10.23, true, 0),
 ('Gabumon', '2018-11-15', 8, true, 2),
